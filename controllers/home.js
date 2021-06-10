3e267c42688f9c535821afcdcb76681c86ce07b8
Donny Ostrianto <donny.ostrianto@gmail.com>
2021-06-10 14:17:07 +0700

    add clock in and clock out functionality

--- controllers/home.js
+++ controllers/home.js
@@ -1,15 +1,16 @@
+const moment = require('moment');
 const User = require('../models/User');
+const Attendance = require('../models/Attendance');
 
-/**
- * GET /
- * Home page.
- */
-exports.index = (req, res, next) => {
+const CLOCK_IN = 'clock in';
+const CLOCK_OUT = 'clock out';
+const FORMAT_DATE = 'YYYY-MM-DD';
+const FORMAT_TIME = 'hh:mm:ss a';
+
+exports.index = async (req, res, next) => {
   if (req.user && req.user.id) {
-    User.findById(req.user.id, (err, user) => {
-      if (err) {
-        return next(err);
-      }
+    try {
+      const user = await User.findById(req.user.id);
       const now = new Date();
       const options = {
         weekday: 'long',
@@ -17,16 +18,64 @@ exports.index = (req, res, next) => {
         month: 'long',
         day: 'numeric'
       };
+      const clockIn = await Attendance.findOne({
+        userId: req.user.id,
+        scheduleDate: moment().format(FORMAT_DATE),
+        clockType: CLOCK_IN
+      });
+      const clockOut = await Attendance.findOne({
+        userId: req.user.id,
+        scheduleDate: moment().format(FORMAT_DATE),
+        clockType: CLOCK_OUT
+      });
       res.render('home', {
         title: 'Home',
         clock: now.toLocaleTimeString(),
         day: now.toLocaleDateString('id-ID', options),
-        user
+        user,
+        clockIn: clockIn ? moment(clockIn.clockAt).format(FORMAT_TIME) : null,
+        clockOut: clockOut ? moment(clockOut.clockAt).format(FORMAT_TIME) : null
       });
-    });
+    } catch (error) {
+      return next(error);
+    }
   } else {
     res.render('home', {
       title: 'Home'
     });
+  }
+};
+
+exports.postClockIn = async (req, res, next) => {
+  const attendance = new Attendance({
+    userId: req.user.id,
+    scheduleDate: moment().format(FORMAT_DATE),
+    clockAt: moment(),
+    clockType: CLOCK_IN
+  });
+
+  try {
+    await attendance.save();
+    req.flash('success', { msg: 'Success clock in.' });
+    res.redirect('/');
+  } catch (error) {
+    return next(error);
+  }
+};
+
+exports.postClockOut = async (req, res, next) => {
+  const attendance = new Attendance({
+    userId: req.user.id,
+    scheduleDate: moment().format(FORMAT_DATE),
+    clockAt: moment(),
+    clockType: CLOCK_OUT,
+  });
+
+  try {
+    await attendance.save();
+    req.flash('success', { msg: 'Success clock out.' });
+    res.redirect('/');
+  } catch (error) {
+    return next(error);
   }
 };
